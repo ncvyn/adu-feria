47f2e852714bf2aab34f764082cd3693a4a8fc91
Nevan Catoy <93510179+ncvyn@users.noreply.github.com>
2025-08-09 21:06:22 +0800

    Add .env file, add alpinejs dependency, and lint code

--- src/data.js
+++ src/data.js
@@ -1,84 +1,87 @@
+import Alpine from 'alpinejs';
+window.Alpine = Alpine;
+
 const url = 'redacted';
 
-document.addEventListener('alpine:init', () => {
-  Alpine.data('data', () => ({
-    init() {
-      this.dummyData();
-    },
+Alpine.data('data', () => ({
+  init() {
+    this.dummyData();
+  },
 
-    isLoading: false,
-    responses: [],
+  isLoading: false,
+  responses: [],
 
-    getData() {
-      this.isLoading = true;
-      fetch(url)
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-          return response.json();
-        })
-        .then((data) => {
-          this.responses = data['values'].map(
-            ([
-              timestamp,
-              email,
-              fullName,
-              instEmail,
-              personalEmail,
-              pageLink,
-              handleLink,
-              serviceType,
-              serviceName,
-              description,
-              price,
-              image,
-            ]) => ({
-              timestamp,
-              email,
-              fullName,
-              instEmail,
-              personalEmail,
-              pageLink,
-              handleLink,
-              serviceType,
-              serviceName,
-              description,
-              price,
-              image,
-            }),
-          );
+  getData() {
+    this.isLoading = true;
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        this.responses = data['values'].map(
+          ([
+            timestamp,
+            email,
+            fullName,
+            instEmail,
+            personalEmail,
+            pageLink,
+            handleLink,
+            serviceType,
+            serviceName,
+            description,
+            price,
+            image,
+          ]) => ({
+            timestamp,
+            email,
+            fullName,
+            instEmail,
+            personalEmail,
+            pageLink,
+            handleLink,
+            serviceType,
+            serviceName,
+            description,
+            price,
+            image,
+          })
+        );
 
-          console.log(this.responses);
-          this.isLoading = false;
-        })
-        .catch((error) => {
-          console.log('Error:', error);
-        });
-    },
-
-    dummyData() {
-      this.responses = new Array(10).fill({
-        timestamp: '06/08/2025 13:50:57',
-        email: 'email@email.com',
-        fullName: 'fname',
-        instEmail: 'iemail',
-        personalEmail: 'persemail',
-        pageLink: 'fbacc',
-        handleLink: 'handle',
-        serviceType: 'Others',
-        serviceName: 'business',
-        description: 'description',
-        price: 'Php 457',
-        image: '',
+        console.log(this.responses);
+        this.isLoading = false;
+      })
+      .catch((error) => {
+        console.log('Error:', error);
       });
-    },
+  },
 
-    setPath(path) {
-      history.pushState(null, '', path)
-      this.currentPath = path
-    },
+  dummyData() {
+    this.responses = new Array(10).fill({
+      timestamp: '06/08/2025 13:50:57',
+      email: 'email@email.com',
+      fullName: 'fname',
+      instEmail: 'iemail',
+      personalEmail: 'persemail',
+      pageLink: 'fbacc',
+      handleLink: 'handle',
+      serviceType: 'Others',
+      serviceName: 'business',
+      description: 'description',
+      price: 'Php 457',
+      image: '',
+    });
+  },
 
-    currentPath: '',
-  }));
-});
+  setPath(path) {
+    history.pushState(null, '', path);
+    this.currentPath = path;
+  },
+
+  currentPath: '',
+}));
+
+Alpine.start();
